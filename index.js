4bf85d68d3e112fefa6894b5c5a1195bf7a68d6d
Brandon Choy <91912156+bchoy11@users.noreply.github.com>
2021-10-07 00:44:46 -0500

    aligned grid layout

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function displayTime(id){
     var x='time-report-'+id;
     var totaldivs=document.querySelectorAll('.time-report-day,.time-report-week,.time-report-month');
+    var curbutton=document.getElementsByClassName("time-button");
     for(var i=0;i<totaldivs.length;i++){
         var curdiv=totaldivs[i];
         if(curdiv.className===x){
-            curdiv.style.display="block"
+            curdiv.style.display="block";
         } 
         else{
-            curdiv.style.display="none"
+            curdiv.style.display="none";
         }
     }
+    for(var t=0;t<curbutton.length;t++){
+        curbutton[t].style.color="hsl(235, 45%, 61%)";
+    }
+    document.getElementById(id+'-button').style.color="#fff";
 }
